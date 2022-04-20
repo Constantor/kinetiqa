e370d06eca779abb5289de3a5f0e7edb30ba2c52
Stepan Konstantinov <stepan@konstantinov.pl>
2022-04-20 20:15:07 +0300

    Android initialization

---
+++ KinetiqaAndroid/app/src/main/java/bio/kinetiqa/android/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package bio.kinetiqa.android.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+	private val _text = MutableLiveData<String>().apply {
+		value = "This is notifications Fragment"
+	}
+	val text: LiveData<String> = _text
+}
