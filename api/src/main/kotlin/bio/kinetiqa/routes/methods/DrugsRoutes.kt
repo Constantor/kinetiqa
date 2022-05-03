7fd5522093178ba6ceec95af48099afe517d8aa3
Stepan Konstantinov <stepan@konstantinov.pl>
2022-05-03 22:08:01 +0300

    Minor

--- api/src/main/kotlin/bio/kinetiqa/routes/methods/DrugsRoutes.kt
+++ api/src/main/kotlin/bio/kinetiqa/routes/methods/DrugsRoutes.kt
@@ -3,11 +3,14 @@ package bio.kinetiqa.routes.methods
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import bio.kinetiqa.core.utils.Params
+import io.ktor.http.*
+import io.ktor.server.response.*
 
 fun Route.drugsRouting() {
-	route("/method/drugs.list") {
+	route("/drugs.list") {
 		get {
 			val get: Map<String, String> = Params.get(call)
+			call.respond(HttpStatusCode.OK, "List\n${get}")
 		}
 	}
 }
