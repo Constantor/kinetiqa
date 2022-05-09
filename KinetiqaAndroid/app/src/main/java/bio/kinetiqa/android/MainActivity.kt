6417eda8d3847e5e3952a19191c9f7bd94989c7f
Roman Vsemirnov <roman.vsemirnov@yandex.ru>
2022-05-09 16:57:21 +0300

    finished list of subst + progress with left navigation bar

--- KinetiqaAndroid/app/src/main/java/bio/kinetiqa/android/MainActivity.kt
+++ KinetiqaAndroid/app/src/main/java/bio/kinetiqa/android/MainActivity.kt
@@ -1,5 +1,6 @@
 package bio.kinetiqa.android
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.widget.AdapterView.OnItemClickListener
@@ -7,11 +8,13 @@ import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import bio.kinetiqa.android.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +22,7 @@ class MainActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityMainBinding
 	var substances : MutableList<Substance> = ArrayList()
 	lateinit var subList : ListView
+	private lateinit var appBarConfiguration: AppBarConfiguration
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -31,7 +35,7 @@ class MainActivity : AppCompatActivity() {
 		val navController = findNavController(R.id.nav_host_fragment_activity_main)
 		// Passing each menu ID as a set of Ids because each
 		// menu should be considered as top level destinations.
-		val appBarConfiguration = AppBarConfiguration(
+		var appBarConfiguration = AppBarConfiguration(
 			setOf(
 				R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
 			)
@@ -39,29 +43,20 @@ class MainActivity : AppCompatActivity() {
 		setupActionBarWithNavController(navController, appBarConfiguration)
 		navView.setupWithNavController(navController)
 
-		// some unfinished sheet
+		// Not working sheet
 
-/*		setContentView(R.layout.fragment_dashboard)
+		val host: NavHostFragment = supportFragmentManager
+			.findFragmentById(R.id.navFragment) as NavHostFragment? ?: return
+		val navLeftController = host.navController
 
-	    setSubstData()// TODO: хардкод
-		subList = findViewById(R.id.subList)
-		if (subList == null) { Log.e("sublist", "sublist is null")}
-		val substAdapter = SubstanceAdapter(this, R.layout.list_item, substances)
-		subList.adapter = substAdapter*/
+		val sideBar = findViewById<NavigationView>(R.id.nav_left_view)
+		sideBar?.setupWithNavController(navLeftController)
 
-/*		val itemListener =
-			OnItemClickListener { parent, v, position, id -> // получаем выбранный пункт
-				val selectedState: Substance = parent.getItemAtPosition(position) as Substance
-				Toast.makeText(
-					applicationContext, "Был выбран пункт " + selectedState.name,
-					Toast.LENGTH_SHORT
-				).show()
-			}
 
-		subList.setOnItemClickListener(itemListener)*/
-	}
-
-	fun setSubstData() {
-		substances.add(Substance("Каменный уголь", "аыыыаыа", R.drawable.test_photo))
+/*		appBarConfiguration = AppBarConfiguration(navController.graph
+			, drawerLayout = drawer_layout) // для бокового меню
+		val toolBar = findViewById<Toolbar>(R.id.toolbar)
+		setSupportActionBar(toolBar) // для верхнего меню
+		toolBar.setupWithNavController(navController, appBarConfiguration)*/
 	}
 }
--- KinetiqaAndroid/app/src/main/java/bio/kinetiqa/android/ui/dashboard/DashboardFragment.kt
+++ KinetiqaAndroid/app/src/main/java/bio/kinetiqa/android/ui/dashboard/DashboardFragment.kt
@@ -3,12 +3,17 @@ package bio.kinetiqa.android.ui.dashboard
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.ListView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
+import bio.kinetiqa.android.MainActivity
 import bio.kinetiqa.android.R
 import bio.kinetiqa.android.Substance
 import bio.kinetiqa.android.SubstanceAdapter
@@ -32,17 +37,31 @@ class DashboardFragment : Fragment() {
 		container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
-		dashboardViewModel =
-			ViewModelProvider(this).get(DashboardViewModel::class.java)
+		dashboardViewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
 
 		_binding = FragmentDashboardBinding.inflate(inflater, container, false)
 		val root: View = binding.root
 
-		val view: View = inflater.inflate(R.layout.fragment_dashboard, container, false)
+		//val view: View = inflater.inflate(R.layout.fragment_dashboard, container, false)
 		setSubstData()// TODO: хардкод
-		subList = view.findViewById(R.id.subList)
-		val substAdapter = SubstanceAdapter(container?.context ?: activity, R.layout.list_item, substances)
+		subList = root.findViewById(R.id.subList)
+		val context : Context? = this.context
+		if (!isAdded) { Log.e("check_activity", "WTF why everything is not working") }
+		val substAdapter = SubstanceAdapter(activity, R.layout.list_item, substances)
 		subList.adapter = substAdapter
+
+		/// TODO: вероятно поп-ап с подробным окном лекарства
+				val itemListener =
+					AdapterView.OnItemClickListener { parent, v, position, id -> // получаем выбранный пункт
+						val selectedState: Substance =
+							parent.getItemAtPosition(position) as Substance
+						Toast.makeText(
+							activity?.applicationContext, "Был выбран пункт " + selectedState.name,
+							Toast.LENGTH_SHORT
+						).show()
+					}
+		subList.setOnItemClickListener(itemListener)
+		///
 
 		return root
 	}
