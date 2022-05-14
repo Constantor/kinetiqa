62a046ca241e880fb396adfe68feed5bbe9028f4
Stepan Konstantinov <stepan@konstantinov.pl>
2022-05-14 18:07:09 +0300

    Drugs list

--- api/src/main/kotlin/bio/kinetiqa/core/DatabaseFactory.kt
+++ api/src/main/kotlin/bio/kinetiqa/core/DatabaseFactory.kt
@@ -12,11 +12,6 @@ import org.jetbrains.exposed.sql.SchemaUtils.create
 object DatabaseFactory {
 	fun init() {
 		Database.connect("jdbc:postgresql://kinetiqa-postgresql-db-do-user-2264611-0.b.db.ondigitalocean.com:25060/kinetiqa", driver = "org.postgresql.Driver", user = "doadmin", password = "AVNS_Xwx137YDBXY3dXL") // ?sslmode=require
-		transaction {
-			addLogger(StdOutSqlLogger)
-			val query: Query = Drugs.selectAll()
-			println(query.toList())
-		}
 		/*Database.connect(hikari())
 		transaction {
 			create(Users)
--- api/src/main/kotlin/bio/kinetiqa/routes/methods/DrugsRoutes.kt
+++ api/src/main/kotlin/bio/kinetiqa/routes/methods/DrugsRoutes.kt
@@ -3,14 +3,27 @@ package bio.kinetiqa.routes.methods
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import bio.kinetiqa.core.utils.Params
+import bio.kinetiqa.models.Drugs
 import io.ktor.http.*
 import io.ktor.server.response.*
+import org.jetbrains.exposed.sql.Query
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Route.drugsRouting() {
 	route("/drugs.list") {
 		get {
 			val get: Map<String, String> = Params.get(call)
-			call.respond(HttpStatusCode.OK, "List\n${get}")
+			//call.respond(HttpStatusCode.OK, "List\n${get}")
+			var outText: String = ""
+			transaction {
+				addLogger(StdOutSqlLogger)
+				val query: Query = Drugs.selectAll()
+				outText = query.toList().toString()
+			}
+			call.respond(HttpStatusCode.OK, outText)
 		}
 	}
 }
