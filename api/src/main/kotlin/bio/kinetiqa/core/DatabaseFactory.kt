c170a0653aa0fb9a9d2b6968f81f79a945996020
Stepan Konstantinov <stepan@konstantinov.pl>
2022-05-15 14:46:38 +0300

    Fix

--- api/src/main/kotlin/bio/kinetiqa/core/DatabaseFactory.kt
+++ api/src/main/kotlin/bio/kinetiqa/core/DatabaseFactory.kt
@@ -4,6 +4,12 @@ import org.jetbrains.exposed.sql.*
 
 object DatabaseFactory {
 	fun init() {
-		Database.connect("jdbc:postgresql://private-kinetiqa-postgresql-db-do-user-2264611-0.b.db.ondigitalocean.com:25060/kinetiqa?ssl=true&sslmode=require", driver = "org.postgresql.Driver", user = "apiuser", password = "AVNS_TjDHRPxTKdDvbj4")
+		val user: String = System.getenv("DB_USER").takeUnless { it.isNullOrEmpty() } ?: "doadmin";
+		val pass: String = System.getenv("DB_USER").takeUnless { it.isNullOrEmpty() } ?: "AVNS_Xwx137YDBXY3dXL";
+		val host: String = System.getenv("DB_HOST").takeUnless { it.isNullOrEmpty() } ?: "kinetiqa-postgresql-db-do-user-2264611-0.b.db.ondigitalocean.com"
+		val port: Int = 25060
+		val database: String = "kinetiqa"
+
+		Database.connect("jdbc:postgresql://${host}:${port}/${database}?ssl=true&sslmode=require", driver = "org.postgresql.Driver", user = user, password = pass)
 	}
 }
