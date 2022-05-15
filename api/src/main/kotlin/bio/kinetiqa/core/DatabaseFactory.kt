ec00f8f7be32eb656ca8bffa833d5a5dd8e821d9
Stepan Konstantinov <stepan@konstantinov.pl>
2022-05-15 15:00:31 +0300

    Env vars for DB auth (we haven't changed pass still)

--- api/src/main/kotlin/bio/kinetiqa/core/DatabaseFactory.kt
+++ api/src/main/kotlin/bio/kinetiqa/core/DatabaseFactory.kt
@@ -1,11 +1,15 @@
 package bio.kinetiqa.core
 
 import org.jetbrains.exposed.sql.*
+import kotlin.Exception;
 
 object DatabaseFactory {
 	fun init() {
-		val user: String = System.getenv("DB_USER").takeUnless { it.isNullOrEmpty() } ?: "doadmin"
-		val pass: String = System.getenv("DB_PASS").takeUnless { it.isNullOrEmpty() } ?: "AVNS_Xwx137YDBXY3dXL"
+		val user: String? = System.getenv("DB_USER").takeUnless { it.isNullOrEmpty() }
+		val pass: String? = System.getenv("DB_PASS").takeUnless { it.isNullOrEmpty() }
+		if(user.isNullOrEmpty() || pass.isNullOrEmpty()) {
+			throw Exception("Missing auth credentials")
+		}
 		val host: String = System.getenv("DB_HOST").takeUnless { it.isNullOrEmpty() } ?: "kinetiqa-postgresql-db-do-user-2264611-0.b.db.ondigitalocean.com"
 		val port: Int = 25060
 		val database: String = "kinetiqa"
