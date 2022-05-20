254b46e29afec7110d806b77429c41072aeedd61
Ilya <ilya.brovarnik@gmail.com>
2022-05-21 02:52:39 +0300

    try fixing

--- api/src/main/kotlin/bio/kinetiqa/model/models/Drug.kt
+++ api/src/main/kotlin/bio/kinetiqa/model/models/Drug.kt
@@ -1,15 +1,17 @@
 package bio.kinetiqa.model.models
 
 import org.ktorm.entity.Entity
+import kotlinx.serialization.*
 
-interface Drug : Entity<Drug> {
+@Serializable
+abstract class Drug : Entity<Drug> {
     companion object : Entity.Factory<Drug>()
-    val id: Int
-    val labelName: String
-    val iupac: String
-    val description: String
-    val kineticsPlot: String
-    val photoURL: String
-    val standardDosageMG: Double
-    val dosageStepMG: Double
+    abstract val id: Int
+    abstract val labelName: String
+    abstract val iupac: String
+    abstract val description: String
+    abstract val kineticsPlot: String
+    abstract val photoURL: String
+    abstract val standardDosageMG: Double
+    abstract val dosageStepMG: Double
 }
