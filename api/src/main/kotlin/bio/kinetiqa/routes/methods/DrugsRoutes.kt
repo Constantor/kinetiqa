40e5a5eb4d350a4dbebab51c1817b1894c1f421c
Броварник Илья Алексеевич <ilya.brovarnik@gmail.com>
2022-05-26 01:28:35 +0300

    try different serialization option

--- api/src/main/kotlin/bio/kinetiqa/routes/methods/DrugsRoutes.kt
+++ api/src/main/kotlin/bio/kinetiqa/routes/methods/DrugsRoutes.kt
@@ -5,6 +5,7 @@ import io.ktor.server.routing.*
 import bio.kinetiqa.core.utils.Params
 import bio.kinetiqa.model.dataclasses.Drug
 import bio.kinetiqa.model.tables.Drugs
+import com.google.gson.Gson
 import io.ktor.http.*
 import io.ktor.server.response.*
 import org.jetbrains.exposed.sql.*
@@ -13,10 +14,11 @@ import org.jetbrains.exposed.sql.transactions.transaction
 fun Route.drugsRouting() {
 	route("/drugs.list") {
 		get {
+			val gson = Gson()
 			val get: Map<String, String> = Params.get(call)
 			val out = transaction {
 				addLogger(StdOutSqlLogger)
-				Drug.all().toList()
+				Drug.all().map { drug -> gson.toJson(drug) }
 			}
 			call.respond(HttpStatusCode.OK, out)
 		}
