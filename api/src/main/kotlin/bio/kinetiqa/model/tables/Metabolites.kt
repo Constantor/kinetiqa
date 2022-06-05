58eba0eb2c13099b3fa7ff9787883ed7dabfc1f5
Броварник Илья Алексеевич <ilya.brovarnik@gmail.com>
2022-06-05 19:01:53 +0300

    update tables

--- api/src/main/kotlin/bio/kinetiqa/model/tables/Metabolites.kt
+++ api/src/main/kotlin/bio/kinetiqa/model/tables/Metabolites.kt
@@ -3,8 +3,8 @@ package bio.kinetiqa.model.tables
 import org.jetbrains.exposed.dao.id.IntIdTable
 
 object Metabolites : IntIdTable() {
-    val name = text("label_name").uniqueIndex()
-    val iupac = text("iupac").uniqueIndex()
+    val name = varchar("label_name", 100).uniqueIndex()
+    val iupac = varchar("iupac", 1000).uniqueIndex()
     val description = text("description")
     val kineticsPlot = text("kinetics_plot")
     val photoURL = text("photo_url")
