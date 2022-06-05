265d63740c5c9e55f52f71a3a29f58939c15d1d7
Броварник Илья Алексеевич <ilya.brovarnik@gmail.com>
2022-06-05 14:35:47 +0300

    add EntityID serializer

--- api/src/main/kotlin/bio/kinetiqa/plugins/Serialization.kt
+++ api/src/main/kotlin/bio/kinetiqa/plugins/Serialization.kt
@@ -1,6 +1,5 @@
 package bio.kinetiqa.plugins
 
-import bio.kinetiqa.model.dataclasses.Drug
 import com.google.gson.JsonObject
 import com.google.gson.JsonSerializer
 import io.ktor.serialization.gson.*
@@ -15,17 +14,22 @@ import kotlin.reflect.full.memberProperties
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
         gson {
-            val serializer: JsonSerializer<Entity<*>> =
+            val entitySerializer: JsonSerializer<Entity<*>> =
                 JsonSerializer<Entity<*>> { src, _, context ->
                     val jsonObject = JsonObject()
                     val id = src::class.memberProperties.find { it.name == "id" }!!.getter.call(src)!!
-                    jsonObject.add("id", context.serialize(id::class.memberProperties.find { it.name == "value" }!!.getter.call(id)))
+                    jsonObject.add("id", context.serialize(id))
                     for(property in src::class.declaredMemberProperties) {
                         jsonObject.add(property.name, context.serialize(property.getter.call(src)))
                     }
                     jsonObject
                 }
-            registerTypeHierarchyAdapter(Entity::class.java, serializer)
+            val entityIdSerializer: JsonSerializer<EntityID<*>> =
+                JsonSerializer<EntityID<*>> { src, _, context ->
+                    context.serialize(src::class.memberProperties.find { it.name == "value" }!!.getter.call(src))
+                }
+            registerTypeHierarchyAdapter(Entity::class.java, entitySerializer)
+            registerTypeHierarchyAdapter(EntityID::class.java, entityIdSerializer)
         }
     }
 }
