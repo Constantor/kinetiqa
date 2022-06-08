c70e3a6fac600adfd4938027deae34c3d966ac0c
Броварник Илья Алексеевич <ilya.brovarnik@gmail.com>
2022-06-08 19:17:17 +0300

    fix more bugs

--- api/src/main/kotlin/bio/kinetiqa/routes/methods/AuthRoutes.kt
+++ api/src/main/kotlin/bio/kinetiqa/routes/methods/AuthRoutes.kt
@@ -13,49 +13,54 @@ import io.ktor.server.response.*
 import io.ktor.server.sessions.*
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Route.authRouting() {
-	route("/sign.up") {
-		post {
-			val params = call.receiveParameters()
-			val user = User.new {
-				email = params["email"]!!
-				passwordHash = params["password"]!!
-			}
-			call.sessions.set(UserSession(user.id.value))
-			call.respond(HttpStatusCode.OK, "Sign up successful")
-		}
-	}
+    route("/sign.up") {
+        post {
+            val params = call.receiveParameters()
+            val user = transaction {
+                User.new {
+                    email = params["email"]!!
+                    passwordHash = params["password"]!!
+                }
+            }
+            call.sessions.set(UserSession(user.id.value))
+            call.respond(HttpStatusCode.OK, "Sign up successful")
+        }
+    }
 
-	route("/sign.in") {
-		post {
-			try {
-				val params = call.receiveParameters()
-				val user = User.find(Users.email eq params["email"]!!).first()
-				if (user.passwordHash == params["password"]!!) {
-					call.sessions.set(UserSession(user.id.value))
-				} else {
-					call.respond(HttpStatusCode.OK, "Wrong password")
-				}
-			} catch (e: NoSuchElementException) {
-				call.respond(HttpStatusCode.OK, "No such user")
-			}
-			call.respond(HttpStatusCode.OK, "Sign in successful")
-		}
-	}
+    route("/sign.in") {
+        post {
+            try {
+                val params = call.receiveParameters()
+                val user = transaction {
+                    User.find(Users.email eq params["email"]!!).first()
+                }
+                if (user.passwordHash == params["password"]!!) {
+                    call.sessions.set(UserSession(user.id.value))
+                } else {
+                    call.respond(HttpStatusCode.OK, "Wrong password")
+                }
+            } catch (e: NoSuchElementException) {
+                call.respond(HttpStatusCode.OK, "No such user")
+            }
+            call.respond(HttpStatusCode.OK, "Sign in successful")
+        }
+    }
 
-	authenticate("auth-session") {
-		route("/test") {
-			get {
-				call.respond(HttpStatusCode.OK, "You are logged in ${call.principal<UserSession>()!!.id}")
-			}
-		}
-	}
+    authenticate("auth-session") {
+        route("/test") {
+            get {
+                call.respond(HttpStatusCode.OK, "You are logged in ${call.principal<UserSession>()!!.id}")
+            }
+        }
+    }
 
-	route("/logout") {
-		get {
-			call.sessions.clear<UserSession>()
-			call.respond(HttpStatusCode.OK, "Logout successful")
-		}
-	}
+    route("/logout") {
+        get {
+            call.sessions.clear<UserSession>()
+            call.respond(HttpStatusCode.OK, "Logout successful")
+        }
+    }
 }
