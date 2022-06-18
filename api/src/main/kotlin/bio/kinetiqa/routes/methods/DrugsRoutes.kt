1df14d2060b6aa22d573e1055801a10f46f8b6fb
Броварник Илья Алексеевич <ilya.brovarnik@gmail.com>
2022-06-18 19:00:00 +0300

    add intakes.list

--- api/src/main/kotlin/bio/kinetiqa/routes/methods/DrugsRoutes.kt
+++ api/src/main/kotlin/bio/kinetiqa/routes/methods/DrugsRoutes.kt
@@ -6,6 +6,7 @@ import bio.kinetiqa.model.entities.Intake
 import bio.kinetiqa.model.sessions.UserSession
 import bio.kinetiqa.model.tables.Courses
 import bio.kinetiqa.model.tables.Drugs
+import bio.kinetiqa.model.tables.Intakes
 import bio.kinetiqa.model.tables.Users
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -21,36 +22,60 @@ import java.time.LocalDateTime
 import java.time.ZoneOffset
 
 fun Route.drugsRouting() {
-	route("/drugs.list") {
-		get {
-			val out = transaction {
-				Drug.all().orderBy(Drugs.labelName to SortOrder.ASC).toList()
-			}
-			call.respond(HttpStatusCode.OK, out)
-		}
-	}
+    route("/drugs.list") {
+        get {
+            val out = transaction {
+                Drug.all().orderBy(Drugs.labelName to SortOrder.ASC).toList()
+            }
+            call.respond(HttpStatusCode.OK, out)
+        }
+    }
 
-	authenticate("auth-session") {
-		route("/intakes.add") {
-			post {
-				val curUserId = call.principal<UserSession>()!!.userId
-				try {
-					val params = call.receiveParameters()
-					val curDrugId = params["drug_id"]!!.toInt()
-					transaction {
-						Intake.new {
-							userId = EntityID(curUserId, Users)
-							drugId = EntityID(curDrugId, Drugs)
-							massIntookMg = Drug[curDrugId].standardDosageMG.toInt()
-							timeWhen = LocalDateTime.now().toInstant(ZoneOffset.UTC)
-						}
-					}
-				} catch (e: NullPointerException) {
-					call.respond(HttpStatusCode.BadRequest, "Invalid request parameters")
-					return@post
-				}
-				call.respond(HttpStatusCode.OK, "Intake add successful")
-			}
-		}
-	}
+    authenticate("auth-session") {
+        route("/intakes.add") {
+            post {
+                val curUserId = call.principal<UserSession>()!!.userId
+                try {
+                    val params = call.receiveParameters()
+                    val curDrugId = params["drug_id"]!!.toInt()
+                    transaction {
+                        Intake.new {
+                            userId = EntityID(curUserId, Users)
+                            drugId = EntityID(curDrugId, Drugs)
+                            massIntookMg = Drug[curDrugId].standardDosageMG.toInt()
+                            timeWhen = LocalDateTime.now().toInstant(ZoneOffset.UTC)
+                        }
+                    }
+                } catch (e: NullPointerException) {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid request parameters")
+                    return@post
+                }
+                call.respond(HttpStatusCode.OK, "Intake add successful")
+            }
+        }
+
+        route("/intakes.list") {
+            get {
+                val curUserId = call.principal<UserSession>()!!.userId
+                val out: List<Intake>
+                try {
+                    val params = call.receiveParameters()
+                    val curDrugId = params["drug_id"]!!.toInt()
+                    out = transaction {
+                        Intakes.select((Courses.userId eq curUserId) and (Courses.drugId eq curDrugId))
+                            .orderBy(Intakes.timeWhen to SortOrder.ASC)
+                            .map { row -> Intake.wrapRow(row) }.filter { intake ->
+                                intake.timeWhen.isAfter(
+                                    LocalDateTime.now().minusWeeks(1).toInstant(ZoneOffset.UTC)
+                                )
+                            }.toList()
+                    }
+                } catch (e: NullPointerException) {
+                    call.respond(HttpStatusCode.BadRequest, "Invalid request parameters")
+                    return@get
+                }
+                call.respond(HttpStatusCode.OK, out)
+            }
+        }
+    }
 }
